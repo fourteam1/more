198057667e2548edf560ee9fa0cdb3010276765b
guohongpengz <1034458037@qq.com>
2019-06-11 17:27:48 +0800

    更改了shop文件

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    data: []
   },
 
   /**
@@ -15,8 +15,25 @@ Page({
     wx.setNavigationBarTitle({
       title: '购物车',
     })
+    this.getData();
   },
-
+  getData(){
+    wx.request({
+      url: "http://mobile.yangkeduo.com/proxy/api/api/alexa/v1/goods?list_update_time=true&platform=1&assist_allowed=1&page=1&size=20&list_id=40IGMQwe8t&antiContent=0anAfxn5ryloU9dVzi6xXe7XBEYGZ02u1eRp6zsG0p24VBvHZGpuUI6FIe94e_szamrCZNIUwuomrRnx7uxmVT9d9LTTlzjQ1gQvxZfGctxGlJYDe6LXVv1aDnbPHfnICDbTCXJwjMm9Qpp9BdGyYZt_NlYDEmkq0PPX7zp8vGGfFwM5s46ju2gHErdC2cY_0gqZw6eC56DfJeoP-bD0clVISh9lfKoVKk2UOzxbLdYYz3fE9U6vPKhnNRGnlzkKwGmU1XJqrTNLSRArAM6oqB-ugeotVl27wJm0kinAYzwZSMo2LGdDBt1v0dEDCjA8OqMqSFNsgB_ORB6ztyFM--dyC7c881nz5j51iXa5RVtMcGTTuz3bw7VKYW-crokveUvvFbRN5SYo-s8RwfrqpAp3dXZKhOrdOlDrORYGosHDKP&pdduid=0",
+      header: {
+        "content-type": "application/json"
+      },
+      success: res => {
+        console.log(res.data.goods_list);
+        this.setData({
+          data: res.data.goods_list
+        })
+      },
+      fail: err => {
+        console.log("错误内容 " + err);
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
