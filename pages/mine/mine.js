4aaae8a42580126b88f0a0bec3ecb4c5e291c3a3
serect1107 <2074255835@qq.com>
2019-06-14 09:29:05 +0800

    修改了首页 样式

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -12,10 +12,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarColor({
-      frontColor: '#ffffff',
-      backgroundColor: '#1ca95e',
-    })
     wx.setNavigationBarTitle({
       title: '我的',
     })
