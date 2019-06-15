5db3f8d2fbfb8ffe01dd7cef441ccb23d52d79a1
guohongpengz <103445037@qq.com>
2019-06-15 14:12:49 +0800

    更改了shop

--- pages/order/order.js
+++ pages/order/order.js
@@ -10,17 +10,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-<<<<<<< HEAD
-  onLoad: function (options) {
-    this.getdata();
-  },
-  getdata(){
-=======
   onLoad: function(options) {
     this.getdata();
   },
   getdata() {
->>>>>>> b033ac3b477e918b5ebc9d1ff4848ef145c8eaf9
     let data = wx.getStorageSync("data");
     console.log(data)
   }
